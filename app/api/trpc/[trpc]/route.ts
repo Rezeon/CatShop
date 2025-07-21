fb1e134dbdcf754a3b9c2524059ae1d775785a44
Rezeon <Rheynoair@gmail.com>
2025-07-21 14:02:23 +0700

    fix bug deploy

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -2,12 +2,14 @@ import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { appRouter } from '@/scr/server/trpc/routers';
 import { createContext } from '@/scr/server/trpc/context';
 
-const handler = (req: Request) =>
-  fetchRequestHandler({
+export const GET = handler;
+export const POST = handler;
+
+async function handler(req: Request): Promise<Response> {
+  return fetchRequestHandler({
     endpoint: '/api/trpc',
     req,
     router: appRouter,
     createContext,
   });
-
-export { handler as GET, handler as POST };
+}
